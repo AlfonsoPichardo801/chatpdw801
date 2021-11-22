639906e7e5aaad915e6a3af5ba5b631eb8844dad
AlfonsoPichardo801 <94817366+AlfonsoPichardo801@users.noreply.github.com>
2021-11-21 22:55:50 -0600

    Add files via upload
    Agregar Archivos

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyB6iFL4Luj-4tXF7AySimkn5uVYodIStMA",
+  authDomain: "chatpdw801apv.firebaseapp.com",
+  projectId: "chatpdw801apv",
+  storageBucket: "chatpdw801apv.appspot.com",
+  messagingSenderId: "1048912276939",
+  appId: "1:1048912276939:web:037e377a675a3573a49a4e",
+  measurementId: "G-JM0BZKM1H6"
+    });
